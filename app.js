2d823144db68c6079430a0115422d9ee5f99440a
dasunpubudumal <dasun.15@cse.mrt.ac.lk>
2017-08-11 18:18:27 +0530

    Moved 'fortune cookie' functionality into a module

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var fs = require('fs');
+var fortune = require('./lib/fortune.js');
 var handlebars = require('express3-handlebars').create({ defaultLayout: 'main' });  //view engine will be handlebars.
 //Layout will be in the 'main' handlebars file.
 app.engine('handlebars', handlebars.engine);    //Setting up the view engine - handlebars.
@@ -12,23 +13,13 @@ app.use(express.static(__dirname + '/public'));  //The 'static' middleware allow
 //Now that we have added the middleware, we can't type /public when addressing anymore as this directory 
 //is invisible to the client.
 
-var fortunes = [
-    "Conquer your fears or they will conquer you.",
-    "Rivers need springs.",
-    "Do not fear what you don't know.",
-    "You will have a pleasant surprise.",
-    "Whenever possible, keep it simple."
-];
 
 app.get('/', function (req, res) {
     res.render('home');
 });
 
 app.get('/about', function (req, res) {
-    var randomFortune = 
-        fortunes[Math.floor((Math.random() * fortunes.length))];
-    var myName = "Dasun";
-    res.render('about', {fortune: randomFortune, name: myName});
+    res.render('about', { fortune:fortune.getFortune() });
 });
 
 // custom 404 page
